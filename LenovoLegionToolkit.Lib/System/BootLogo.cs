3c99968ee3d488928bc780a371ce2e3050048b43
辉夜 <rx700@vip.qq.com>
2026-06-23 18:22:08 +0800

    Fixed an issue where Boot Logo is invalid on new generation laptops.

--- LenovoLegionToolkit.Lib/System/BootLogo.cs
+++ LenovoLegionToolkit.Lib/System/BootLogo.cs
@@ -1,14 +1,14 @@
 using System;
-using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
-using Windows.Win32;
-using Windows.Win32.Foundation;
 using LenovoLegionToolkit.Lib.Extensions;
 using LenovoLegionToolkit.Lib.Utils;
+using Windows.Win32;
+using Windows.Win32.Foundation;
 
 namespace LenovoLegionToolkit.Lib.System;
 
@@ -37,8 +37,6 @@ public static class BootLogo
     {
         try
         {
-
-
             var mi = await Compatibility.GetMachineInformationAsync().ConfigureAwait(false);
             if (!mi.Properties.SupportsBootLogoChange)
                 return false;
@@ -67,11 +65,49 @@ public static class BootLogo
 
         ThrowIfImageInvalid(info, sourcePath);
 
-        await DeleteMyLogoAsync().ConfigureAwait(false);
-        var crc = await CopyMyLogoAsync(info, sourcePath).ConfigureAwait(false);
+        var existingVersion = ReadExistingLBLDVCVersion();
+        var useSha256 = existingVersion < 0 || existingVersion >= SHA256_VERSION;
+        var version = useSha256 ? SHA256_VERSION : existingVersion;
 
-        SetChecksum(crc);
+        Log.Instance.Trace($"Existing LBLDVC version: 0x{existingVersion.ToString("X")}, using SHA256: {useSha256}");
+
         SetInfo(info with { Enabled = 1 });
+
+        char? drive = null;
+        try
+        {
+            drive = await MountEfiPartitionAsync().ConfigureAwait(false);
+            if (!drive.HasValue)
+            {
+                Log.Instance.Trace($"Cannot mount EFI partition.");
+                throw new CantMountUEFIPartitionException();
+            }
+
+            DeleteLogosOnEfiPartition(drive.Value);
+
+            var destinationPath = CopyLogoToEfiPartition(info, sourcePath, drive.Value);
+
+            byte[] hash;
+            if (useSha256)
+            {
+                hash = SHA256.HashData(File.ReadAllBytes(destinationPath));
+            }
+            else
+            {
+                var crc = Crc32Adler.Calculate(destinationPath);
+                hash = new byte[32];
+                BitConverter.GetBytes(crc).CopyTo(hash, 0);
+            }
+
+            WriteChecksum(version, hash);
+        }
+        finally
+        {
+            if (drive.HasValue)
+                await UnMountEfiPartitionAsync(drive.Value).ConfigureAwait(false);
+        }
+
+        await DisableWindowsBootUXAsync().ConfigureAwait(false);
 
         Log.Instance.Trace($"Enabled logo. [sourcePath={sourcePath}]");
     }
@@ -80,10 +116,23 @@ public static class BootLogo
     {
         Log.Instance.Trace($"Disabling logo...");
 
-        await DeleteMyLogoAsync().ConfigureAwait(false);
+        char? drive = null;
+        try
+        {
+            drive = await MountEfiPartitionAsync().ConfigureAwait(false);
+            if (drive.HasValue)
+                DeleteLogosOnEfiPartition(drive.Value);
+        }
+        finally
+        {
+            if (drive.HasValue)
+                await UnMountEfiPartitionAsync(drive.Value).ConfigureAwait(false);
+        }
 
-        SetChecksum(0);
+        WriteChecksum(SHA256_VERSION, new byte[32]);
         SetInfo(GetInfo() with { Enabled = 0 });
+
+        await EnableWindowsBootUXAsync().ConfigureAwait(false);
 
         Log.Instance.Trace($"Disabled logo.");
     }
@@ -105,13 +154,12 @@ public static class BootLogo
 
             var ptrSize = (uint)Marshal.SizeOf<BootLogoInfo>();
 
+            uint size;
             fixed (char* pName = LBLDESP)
             fixed (char* pGuid = LBLDESP_GUID)
-            {
-                var size = PInvoke.GetFirmwareEnvironmentVariableEx(new PCWSTR(pName), new PCWSTR(pGuid), ptr.ToPointer(), ptrSize, null);
-                if (size != ptrSize)
-                    PInvokeExtensions.ThrowIfWin32Error("GetFirmwareEnvironmentVariableEx");
-            }
+                size = PInvoke.GetFirmwareEnvironmentVariableEx(new PCWSTR(pName), new PCWSTR(pGuid), ptr.ToPointer(), ptrSize, null);
+            if (size != ptrSize)
+                PInvokeExtensions.ThrowIfWin32Error("GetFirmwareEnvironmentVariableEx");
 
             var str = Marshal.PtrToStructure<BootLogoInfo>(ptr);
 
@@ -137,7 +185,8 @@ public static class BootLogo
         {
             if (!TokenManipulator.AddPrivileges(TokenManipulator.SE_SYSTEM_ENVIRONMENT_PRIVILEGE))
             {
-                Log.Instance.Trace($"Cannot set UEFI privileges.");
+                if (Log.Instance.IsTraceEnabled)
+                    Log.Instance.Trace($"Cannot set UEFI privileges.");
 
                 throw new CantSetUEFIPrivilegeException();
             }
@@ -145,12 +194,12 @@ public static class BootLogo
             Marshal.StructureToPtr(info, ptr, false);
             var ptrSize = (uint)Marshal.SizeOf<BootLogoInfo>();
 
+            bool result;
             fixed (char* pName = LBLDESP)
             fixed (char* pGuid = LBLDESP_GUID)
-            {
-                if (!PInvoke.SetFirmwareEnvironmentVariableEx(new PCWSTR(pName), new PCWSTR(pGuid), ptr.ToPointer(), ptrSize, SCOPE_ATTR))
-                    PInvokeExtensions.ThrowIfWin32Error("SetFirmwareEnvironmentVariableEx");
-            }
+                result = PInvoke.SetFirmwareEnvironmentVariableEx(new PCWSTR(pName), new PCWSTR(pGuid), ptr.ToPointer(), ptrSize, SCOPE_ATTR);
+            if (!result)
+                PInvokeExtensions.ThrowIfWin32Error("SetFirmwareEnvironmentVariableEx");
 
             Log.Instance.Trace($"Info set. [enabled={info.Enabled}, supportedWidth={info.SupportedWidth}, supportedHeight={info.SupportedHeight}, supportedFormat={(int)info.SupportedFormat}]");
         }
@@ -162,7 +211,7 @@ public static class BootLogo
         }
     }
 
-    private static unsafe uint GetChecksum()
+    private static unsafe BootLogoChecksum GetChecksum()
     {
         Log.Instance.Trace($"Getting checksum...");
 
@@ -173,39 +222,50 @@ public static class BootLogo
             if (!TokenManipulator.AddPrivileges(TokenManipulator.SE_SYSTEM_ENVIRONMENT_PRIVILEGE))
             {
                 Log.Instance.Trace($"Cannot set UEFI privileges.");
-
                 throw new CantSetUEFIPrivilegeException();
             }
 
             var ptrSize = (uint)Marshal.SizeOf<BootLogoChecksum>();
 
+            uint size;
             fixed (char* pName = LBLDVC)
             fixed (char* pGuid = LBLDVC_GUID)
-            {
-                var size = PInvoke.GetFirmwareEnvironmentVariableEx(new PCWSTR(pName), new PCWSTR(pGuid), ptr.ToPointer(), ptrSize, null);
-                if (size != ptrSize)
-                    PInvokeExtensions.ThrowIfWin32Error("GetFirmwareEnvironmentVariableEx");
-            }
+                size = PInvoke.GetFirmwareEnvironmentVariableEx(new PCWSTR(pName), new PCWSTR(pGuid), ptr.ToPointer(), ptrSize, null);
+            if (size != ptrSize)
+                PInvokeExtensions.ThrowIfWin32Error("GetFirmwareEnvironmentVariableEx");
 
-            var checksum = Marshal.PtrToStructure<BootLogoChecksum>(ptr).Crc;
+            var result = Marshal.PtrToStructure<BootLogoChecksum>(ptr);
+            result.Hash ??= new byte[32];
 
-            Log.Instance.Trace($"Retrieved checksum. [checksum={checksum:X2}]");
+            Log.Instance.Trace($"Retrieved checksum. [version=0x{result.Version.ToString("X")}]");
 
-            return checksum;
+            return result;
         }
         finally
         {
             Marshal.FreeHGlobal(ptr);
-
             TokenManipulator.RemovePrivileges(TokenManipulator.SE_SYSTEM_ENVIRONMENT_PRIVILEGE);
         }
     }
 
-    private static unsafe void SetChecksum(uint checksum)
-    {
-        Log.Instance.Trace($"Setting checksum... [checksum={checksum:X2}]");
+    private const int SHA256_VERSION = 0x00020003;
 
-        var str = new BootLogoChecksum { Crc = checksum };
+    private static int ReadExistingLBLDVCVersion()
+    {
+        try
+        {
+            return GetChecksum().Version;
+        }
+        catch
+        {
+            return -1;
+        }
+    }
+
+    private static unsafe void WriteChecksum(int version, byte[] hash)
+    {
+        Log.Instance.Trace($"Writing checksum... [version=0x{version.ToString("X")}]");
+
         var ptr = Marshal.AllocHGlobal(Marshal.SizeOf<BootLogoChecksum>());
 
         try
@@ -213,137 +273,134 @@ public static class BootLogo
             if (!TokenManipulator.AddPrivileges(TokenManipulator.SE_SYSTEM_ENVIRONMENT_PRIVILEGE))
             {
                 Log.Instance.Trace($"Cannot set UEFI privileges.");
-
                 throw new CantSetUEFIPrivilegeException();
             }
 
-            Marshal.StructureToPtr(str, ptr, false);
             var ptrSize = (uint)Marshal.SizeOf<BootLogoChecksum>();
 
+            var str = new BootLogoChecksum { Version = version, Hash = hash };
+            Marshal.StructureToPtr(str, ptr, false);
+
+            bool result;
             fixed (char* pName = LBLDVC)
             fixed (char* pGuid = LBLDVC_GUID)
-            {
-                if (!PInvoke.SetFirmwareEnvironmentVariableEx(new PCWSTR(pName), new PCWSTR(pGuid), ptr.ToPointer(), ptrSize, SCOPE_ATTR))
-                    PInvokeExtensions.ThrowIfWin32Error("SetFirmwareEnvironmentVariableEx");
-            }
+                result = PInvoke.SetFirmwareEnvironmentVariableEx(new PCWSTR(pName), new PCWSTR(pGuid), ptr.ToPointer(), ptrSize, SCOPE_ATTR);
+            if (!result)
+                PInvokeExtensions.ThrowIfWin32Error("SetFirmwareEnvironmentVariableEx");
 
-            Log.Instance.Trace($"Checksum set. [checksum={checksum:X2}]");
+            Log.Instance.Trace($"Checksum written.");
         }
         finally
         {
             Marshal.FreeHGlobal(ptr);
-
             TokenManipulator.RemovePrivileges(TokenManipulator.SE_SYSTEM_ENVIRONMENT_PRIVILEGE);
         }
     }
 
-    private static async Task<uint> CopyMyLogoAsync(BootLogoInfo info, string sourcePath)
+    private static void DeleteLogosOnEfiPartition(char drive)
     {
-        Log.Instance.Trace($"Copying logo... [sourcePath={sourcePath}, enabled={info.Enabled}, supportedWidth={info.SupportedWidth}, supportedHeight={info.SupportedHeight}, supportedFormat={(int)info.SupportedFormat}]");
+        Log.Instance.Trace($"Deleting logos on EFI partition...");
 
-        char? drive = null;
-
-        try
+        var directoryPath = $@"{drive}:\EFI\Lenovo\Logo";
+        if (!Directory.Exists(directoryPath))
         {
-            drive = await MountEfiPartitionAsync().ConfigureAwait(false);
-            if (!drive.HasValue)
-            {
-                Log.Instance.Trace($"Cannot mount EFI partition.");
-
-                throw new CantMountUEFIPartitionException();
-            }
-
-            if (new DriveInfo($"{drive}:").AvailableFreeSpace < new FileInfo(sourcePath).Length)
-            {
-                Log.Instance.Trace($"Not enough free space on EFI partition.");
-
-                throw new NotEnoughSpaceOnUEFIPartitionException();
-            }
-
-            var destinationDirectory = Path.Combine($"{drive}:", "EFI", "Lenovo", "Logo");
-            var filename = $"mylogo_{info.SupportedWidth}x{info.SupportedHeight}{Path.GetExtension(sourcePath)}";
-            var destinationPath = Path.Combine(destinationDirectory, filename);
-
-            Log.Instance.Trace($"Destination path: {destinationPath}");
-
-            Directory.CreateDirectory(destinationDirectory);
-            File.Copy(sourcePath, destinationPath, true);
-
-            var checksum = Crc32Adler.Calculate(destinationPath);
-
-            Log.Instance.Trace($"Logo copied. [checksum={checksum:X2}]");
-
-            return checksum;
+            Log.Instance.Trace($"No logos to delete.");
+            return;
         }
-        finally
-        {
-            if (drive.HasValue)
-                await UnMountEfiPartitionAsync(drive.Value).ConfigureAwait(false);
-        }
+
+        var files = Directory.EnumerateFiles(directoryPath, "mylogo*").ToArray();
+        files.ForEach(File.Delete);
+
+        Log.Instance.Trace($"Logos deleted. [count={files.Length}]");
     }
 
-    private static async Task DeleteMyLogoAsync()
+    private static string CopyLogoToEfiPartition(BootLogoInfo info, string sourcePath, char drive)
     {
-        Log.Instance.Trace($"Deleting logos...");
+        Log.Instance.Trace($"Copying logo to EFI partition... [sourcePath={sourcePath}, drive={drive}]");
 
-        char? drive = null;
-
-        try
+        if (new DriveInfo($"{drive}:").AvailableFreeSpace < new FileInfo(sourcePath).Length)
         {
-            drive = await MountEfiPartitionAsync().ConfigureAwait(false);
-            if (!drive.HasValue)
-            {
-                Log.Instance.Trace($"Cannot mount EFI partition");
-
-                throw new CantMountUEFIPartitionException();
-            }
-
-            var directoryPath = $@"{drive}:\EFI\Lenovo\Logo";
-            if (!Directory.Exists(directoryPath))
-            {
-                Log.Instance.Trace($"No logos to delete.");
-
-                return;
-            }
-
-            var files = Directory.EnumerateFiles(directoryPath, "mylogo*").ToArray();
-            files.ForEach(File.Delete);
-
-            Log.Instance.Trace($"Logos deleted. [count={files.Length}]");
+            Log.Instance.Trace($"Not enough free space on EFI partition.");
+            throw new NotEnoughSpaceOnUEFIPartitionException();
         }
-        finally
+
+        var sourceExtension = DetectActualExtension(sourcePath);
+        var destinationDirectory = Path.Combine($"{drive}:", "EFI", "Lenovo", "Logo");
+        var filename = $"mylogo_{info.SupportedWidth}x{info.SupportedHeight}{sourceExtension}";
+        var destinationPath = Path.Combine(destinationDirectory, filename);
+
+        Log.Instance.Trace($"Destination path: {destinationPath}");
+
+        Directory.CreateDirectory(destinationDirectory);
+        File.Copy(sourcePath, destinationPath, true);
+
+        return destinationPath;
+    }
+
+    private static string DetectActualExtension(string sourcePath)
+    {
+        using var fs = new FileStream(sourcePath, FileMode.Open, FileAccess.Read);
+        using var br = new BinaryReader(fs);
+
+        var magicBytes = br.ReadBytes(4);
+
+        return magicBytes switch
         {
-            if (drive.HasValue)
-                await UnMountEfiPartitionAsync(drive.Value).ConfigureAwait(false);
-        }
+            [0xFF, 0xD8, ..] => ".jpg",
+            [0x89, 0x50, 0x4E, 0x47] => ".png",
+            [0x42, 0x4D, ..] => ".bmp",
+            _ => Path.GetExtension(sourcePath)
+        };
     }
 
     private static void ThrowIfImageInvalid(BootLogoInfo info, string sourcePath)
     {
-        Log.Instance.Trace($"Validating image... [sourcePath={sourcePath}, sourcePath={sourcePath}, enabled={info.Enabled}, supportedWidth={info.SupportedWidth}, supportedHeight={info.SupportedHeight}, supportedFormat={(int)info.SupportedFormat}]");
+        Log.Instance.Trace($"Validating image... [sourcePath={sourcePath}, enabled={info.Enabled}, supportedWidth={info.SupportedWidth}, supportedHeight={info.SupportedHeight}, supportedFormat={(int)info.SupportedFormat}]");
 
-        using var image = Image.FromFile(sourcePath);
+        var actualExtension = DetectActualExtension(sourcePath);
 
-        if (info.SupportedWidth != image.Width || info.SupportedHeight != image.Height)
+        if (!info.SupportedFormat.ExtensionFilters().Contains($"*{actualExtension}"))
         {
-            Log.Instance.Trace($"Invalid image size.");
-
-            throw new InvalidBootLogoImageSizeException();
-        }
-
-        if (!info.SupportedFormat.ImageFormats().Contains(image.RawFormat))
-        {
-            Log.Instance.Trace($"Invalid image format.");
+            Log.Instance.Trace($"Invalid image format. [actualExtension={actualExtension}]");
 
             throw new InvalidBootLogoImageFormatException();
         }
 
-        Log.Instance.Trace($"Image valid. [sourcePath={sourcePath}, sourcePath={sourcePath}, enabled={info.Enabled}, supportedWidth={info.SupportedWidth}, supportedHeight={info.SupportedHeight}, supportedFormat={(int)info.SupportedFormat}]");
+        Log.Instance.Trace($"Image valid. [sourcePath={sourcePath}, actualExtension={actualExtension}]");
+    }
+
+    private static async Task DisableWindowsBootUXAsync()
+    {
+        Log.Instance.Trace($"Disabling Windows boot animation...");
+
+        try
+        {
+            var (result, _) = await CMD.RunAsync("bcdedit", "-set bootuxdisabled on").ConfigureAwait(false);
+            Log.Instance.Trace($"Disable Windows boot animation result: {result}");
+        }
+        catch (Exception ex)
+        {
+            Log.Instance.Trace($"Failed to disable Windows boot animation. [ex={ex.Message}]");
+        }
+    }
+
+    private static async Task EnableWindowsBootUXAsync()
+    {
+        Log.Instance.Trace($"Enabling Windows boot animation...");
+
+        try
+        {
+            var (result, _) = await CMD.RunAsync("bcdedit", "-set bootuxdisabled off").ConfigureAwait(false);
+            Log.Instance.Trace($"Enable Windows boot animation result: {result}");
+        }
+        catch (Exception ex)
+        {
+            Log.Instance.Trace($"Failed to enable Windows boot animation. [ex={ex.Message}]");
+        }
     }
 
     private static char GetUnusedDriveLetter()
     {
-        // ReSharper disable once StringLiteralTypo
         var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
         var usedLetters = DriveInfo.GetDrives().Select(di => di.Name.First()).ToArray();
 
