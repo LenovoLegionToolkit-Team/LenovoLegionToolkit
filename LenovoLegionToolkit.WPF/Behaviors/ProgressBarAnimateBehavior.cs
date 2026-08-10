61bb679fe821772acd08c25c443876f499f24897
Dr. Skinner <178484933+Metanome@users.noreply.github.com>
2026-08-10 03:08:33 +0300

    Added progress bar animations and accent colors to V2 sensors dashboard

--- LenovoLegionToolkit.WPF/Behaviors/ProgressBarAnimateBehavior.cs
+++ LenovoLegionToolkit.WPF/Behaviors/ProgressBarAnimateBehavior.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -9,6 +10,36 @@ namespace LenovoLegionToolkit.WPF.Behaviors;
 
 public class ProgressBarAnimateBehavior : Behavior<ProgressBar>
 {
+    public static readonly DependencyProperty IsEnabledProperty =
+        DependencyProperty.RegisterAttached("IsEnabled", typeof(bool), typeof(ProgressBarAnimateBehavior), new PropertyMetadata(false, OnIsEnabledChanged));
+
+    public static bool GetIsEnabled(DependencyObject obj) => (bool)obj.GetValue(IsEnabledProperty);
+    public static void SetIsEnabled(DependencyObject obj, bool value) => obj.SetValue(IsEnabledProperty, value);
+
+    private static void OnIsEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not ProgressBar progressBar)
+            return;
+
+        var behaviors = Interaction.GetBehaviors(progressBar);
+        var existing = behaviors.OfType<ProgressBarAnimateBehavior>().FirstOrDefault();
+
+        if ((bool)e.NewValue)
+        {
+            if (existing == null)
+            {
+                behaviors.Add(new ProgressBarAnimateBehavior());
+            }
+        }
+        else
+        {
+            if (existing != null)
+            {
+                behaviors.Remove(existing);
+            }
+        }
+    }
+
     private bool _isAnimating;
 
     protected override void OnAttached()
