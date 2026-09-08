21db3609a0dac7139d33dcbbeeac97a9e352c2f8
Dr. Skinner <178484933+Metanome@users.noreply.github.com>
2026-09-08 17:50:57 +0300

    Updated Legion Space disabler process list with missing background workers

--- LenovoLegionToolkit.Lib/SoftwareDisabler/LegionSpaceDisabler.cs
+++ LenovoLegionToolkit.Lib/SoftwareDisabler/LegionSpaceDisabler.cs
@@ -20,11 +20,20 @@ public class LegionSpaceDisabler : AbstractSoftwareDisabler
 
     protected override IEnumerable<string> ProcessNames =>
     [
+        "Bino3D",
+        "GACapture",
+        "GAController",
+        "GAHighlight",
+        "GAInference",
+        "GAInferOCR",
         "GAService",
         "GAWorker",
         "LegionGameWidget",
         "LegionSpace",
+        "LenovoLighting",
         "LSDaemon",
+        "SEGameHLEditorWorker",
+        "SEGameTool",
         "seworker",
         "SmartEngineHost"
     ];
