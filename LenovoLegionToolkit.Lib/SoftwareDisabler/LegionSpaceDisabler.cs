8f3eb3d2d94d58e10fe6f14a6138af3f2ce73f3f
Dr. Skinner <178484933+Metanome@users.noreply.github.com>
2026-09-08 04:30:34 +0300

    Updated Legion Space disabler services, scheduled tasks, and processes

--- LenovoLegionToolkit.Lib/SoftwareDisabler/LegionSpaceDisabler.cs
+++ LenovoLegionToolkit.Lib/SoftwareDisabler/LegionSpaceDisabler.cs
@@ -1,10 +1,31 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace LenovoLegionToolkit.Lib.SoftwareDisabler;
 
 public class LegionSpaceDisabler : AbstractSoftwareDisabler
 {
-    protected override IEnumerable<string> ScheduledTasksPaths => [];
-    protected override IEnumerable<string> ServiceNames => ["DAService"];
-    protected override IEnumerable<string> ProcessNames => ["LegionSpace", "LSDaemon"];
+    protected override IEnumerable<string> ScheduledTasksPaths =>
+    [
+        "Lenovo\\LegionSpace",
+        "Lenovo\\SmartEngine"
+    ];
+
+    protected override IEnumerable<string> ServiceNames =>
+    [
+        "DAService",
+        "GAService",
+        "LenovoLightingService",
+        "LenovoSmartService"
+    ];
+
+    protected override IEnumerable<string> ProcessNames =>
+    [
+        "GAService",
+        "GAWorker",
+        "LegionGameWidget",
+        "LegionSpace",
+        "LSDaemon",
+        "seworker",
+        "SmartEngineHost"
+    ];
 }
